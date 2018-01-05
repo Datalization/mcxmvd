80ebcd5e9600e5035bd5a08b037d2a93ed2f59dc
yangjiaqing <1826450955@qq.com>
2018-01-05 22:50:03 +0800

    remove return

--- source/myuarm/myuapi.py
+++ source/myuarm/myuapi.py
@@ -6,7 +6,7 @@ def putchess(fieldid):
     global ser,id
     print("hello i will put  chess to %d by uarm,please wait" % fieldid)
     #please add real code There
-    return
+    #return
     id=id+1
     movelist = ['G0 X100 Y100 Z100 F100\n','G0 X65 Y132 Z80 F100\n','G0 X00 Y132 Z80 F100\n','G0 X-55 Y146 Z80 F100\n','G0 X73 Y200 Z80 F100\n','G0 X10 Y200 Z80 F100\n','G0 X-50 Y214 Z80 F100\n','G0 X80 Y268 Z80 F100\n','G0 X10 Y268 Z80 F100\n','G0 X-45 Y282 Z80 F100\n']
     cmdtail = movelist[fieldid]
@@ -18,7 +18,7 @@ def uarminit():
     global ser,id
     id=0
     print("please put uarminit code here")
-    return
+    #return
     ports = list(serial.tools.list_ports.comports())
     print (ports)
     for p in ports:
@@ -28,4 +28,3 @@ def uarminit():
         else :
         # ser = serial.Serial(port="24",baudrate=115200)
             print ("No Serial or SERIAL Device was found connected to the computer")
-
