c7025f00f0a07d31d7ed5cafee9fd3aeeb4eda9c
xknxknqq <1553730177@qq.com>
2018-04-21 23:41:39 +0800

    adapt jpg and bmp

--- source/caffeapp/training/createfilelist.sh
+++ source/caffeapp/training/createfilelist.sh
@@ -2,8 +2,8 @@
 DATA=images
 echo "Create train.txt..."
 rm -rf ./train.txt
-find $DATA -name O*.jpg | cut -d '/' -f2 | sed "s/$/ 1/">>./train.txt
-find $DATA -name X*.jpg | cut -d '/' -f2 | sed "s/$/ 2/">>./tmp.txt
+find $DATA -name O*.*| cut -d '/' -f2 | sed "s/$/ 1/">>./train.txt
+find $DATA -name X*.*| cut -d '/' -f2 | sed "s/$/ 2/">>./tmp.txt
 cat ./tmp.txt>>./train.txt
 rm -rf ./tmp.txt
 echo "Done.."
