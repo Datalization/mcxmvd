a45e4113b2afeb55d0abbdbdbb973573b8abf7b7
xknxknqq <1553730177@qq.com>
2018-04-21 22:12:48 +0800

    remove dummy.txt, add cv2biniunv py

--- docs/cv2bininv.py
+++ docs/cv2bininv.py
@@ -1,11 +1,5 @@
 import cv2  
-import numpy as np  
-import time
 
 img = cv2.imread('zero.bmp',0)
-cv2.imshow('image',img)
-time.sleep(1)
 invimg = ~img
-time.sleep(1)
-cv2.imshow('image',invimg)
 cv2.imwrite('zeroinv.bmp',invimg)
